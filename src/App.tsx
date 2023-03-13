5bb30500c06fb4b63b169606d80b73faa0acbc00
Eunseo Choi <yses9296@gmail.com>
2023-03-13 23:51:51 +0900

    feat: mockData 저장 및 타입 지정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import Chart from "./components/Chart";
+import mockData from "./data/mockData.json";
+import { MockType } from "types/mockData.type";
+
 function App() {
-  return <div>init</div>;
+  const dataJson = JSON.parse(JSON.stringify(mockData.response));
+  const dataArr: MockType[] = Object.keys(dataJson).map(function (_) {
+    return Object.assign({ date: _ }, dataJson[_]);
+  });
+
+  return (
+    <div style={{ width: "100%", height: "100vh" }}>
+      <h3>Flexsys </h3>
+      <Chart data={dataArr} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/types/mockData.type.ts
@@ -0,0 +1,6 @@
+export type MockType = {
+  date: string;
+  id: string;
+  value_area: number;
+  value_bar: number;
+};
